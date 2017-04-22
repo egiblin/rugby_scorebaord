2b09d721013a3f1faa2b167fb3461b2f47fe5751
egiblin <egiblin88@gmail.com>
2017-04-22 16:04:51 -0400

    datetime parse

--- react/src/components/app.js
+++ react/src/components/app.js
@@ -24,6 +24,7 @@ class App extends Component {
         game.key = count;
         count ++;
       });
+      games.reverse();
       this.setState({ fixtures: games});
     });
   }
--- react/src/components/fixture.js
+++ react/src/components/fixture.js
@@ -1,11 +1,28 @@
 import React from 'react';
 
 const Fixture = props => {
-  return (
-    <div>
-      <h3>{props.home_team} at {props.away_team}</h3>
-    </div>
+  let datetime = props.kickoff.split("");
+  let date = datetime.slice(0, 10);
+  date.join();
+  let time = datetime.slice(11, 16);
+  time.join();
+  if (props.home_score == null) {
+    return (
+      <div>
+        <h3>{props.home_team} at {props.away_team}</h3>
+        <h4>{time}</h4>
+        <h5>{date}</h5>
+      </div>
+  );}
+  else {
+    return (
+      <div>
+        <h3>{props.home_team} {props.home_score}-{props.away_score} {props.away_team}</h3>
+        <h4>{time}</h4>
+        <h5>{date}</h5>
+      </div>
   );
+  }
 };
 
 
